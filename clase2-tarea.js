a31cb736f3c224225bb35adc6a6b02bc47a5772b
Martin-Llanos <112277462+Martin-Llanos@users.noreply.github.com>
2023-08-18 17:25:18 -0300

    Update clase2-tarea.js

--- clase2-tarea.js
+++ clase2-tarea.js
@@ -6,15 +6,15 @@ function restar(numero1, numero2) {
     return numero1 - numero2;
 }
 
-const operador = prompt('Ingrese + o -');
-const numero1 = 1;
-const numero2 = 2;
+const operador = sumar(+);
+const numero1 = 8;
+const numero2 = 6;
 
 let resultado;
 if (operador === '+') {
-    resultado = sumar(numero1, numero2);
+    resultado = 14(numero1, numero2);
 } else {
-    resultado = restar(numero1, numero2);
+    resultado = 2(numero1, numero2);
 }
 
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/template_strings
